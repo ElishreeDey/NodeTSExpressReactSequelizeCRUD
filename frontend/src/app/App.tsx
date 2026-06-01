a402bb70d96f7a255fe16f94acae9a047a252cb0
ElishreeDey <elishreed@mindfiresolutions.com>
2026-06-01 16:06:54 +0530

    "docs: change the folder name to app"

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import RenderForm from '../components/Form'
+import RenderTable from '../components/Table'
+
+import useUsers from '../hooks/useUsers'
+
+// These imports are for Toastify pop-ups
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export default function App() {
+  const {
+    tableData,
+    setTableData,
+    editIndex,
+    setEditIndex,
+    editUser,
+    selectedRow,
+    setSelectedRow,
+    handleDelete,
+    handleEdit,
+  } = useUsers()
+
+  return (
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+
+      <div className="pageContainer">
+        <RenderForm
+          tableData={tableData}
+          setTableData={setTableData}
+          editIndex={editIndex}
+          setEditIndex={setEditIndex}
+          editUser={editUser}
+          setSelectedRow={setSelectedRow}
+        />
+
+        <RenderTable
+          tableData={tableData}
+          onDelete={handleDelete}
+          onEdit={handleEdit}
+          selectedRow={selectedRow}
+          setSelectedRow={setSelectedRow}
+        />
+      </div>
+    </>
+  )
+}
